c227cf23ae4c18bc34cb04ec8d104cae671aa70a
JoseLG03 <antares069@gmail.com>
2021-01-27 21:02:43 -0600

    for loop

--- 10-Estructuras/js/01-app.js
+++ 10-Estructuras/js/01-app.js
@@ -5,3 +5,15 @@ for(let i = 0; i <= 20; i++){
         console.log(`El número ${i} es Impar.`);
     }
 }
+
+let carrito = [
+    {nombre:"Monitor de 45 pulgadas", precio:120},
+    {nombre:"Smart TV", precio:200},
+    {nombre:"Switch", precio:700},
+    {nombre:"Camara", precio:1050},
+    {nombre:"Laptop", precio:160}
+];
+
+for( i = 0; i < carrito.length; i++ ){
+    console.log(carrito[i].nombre);
+};
