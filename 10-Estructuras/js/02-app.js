00fc3d8e7dea5257baebf3e3bdf9a2c10b9793ce
JoseLG03 <antares069@gmail.com>
2021-01-27 21:33:28 -0600

    FizzBuzz

---
+++ 10-Estructuras/js/02-app.js
@@ -0,0 +1,20 @@
+//Fizz Buzz al 100
+//multiplos de 3 ...fizz
+//multiplos de 5 ...Buzz
+//multiplos de 15 ...FizzBuzz
+
+for(let i = 1; i <= 100; i++){
+    if(i % 15 === 0){
+        console.log(`Su número es ${i} es FizzBuzz.`);
+    }
+    else if(i % 5 === 0){
+        console.log(`Su número es ${i} es Buzz.`);
+    }
+    else if(i % 3 === 0){
+        console.log(`Su número es ${i} es Fizz.`);    
+    }
+    else{
+        console.log(`Su número es ${i}.`);
+    }
+}
+
