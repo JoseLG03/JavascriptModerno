eb3b8f794ae9d92925ae49a5d86dac940d3bd86c
JoseLG03 <antares069@gmail.com>
2021-01-28 20:56:48 -0600

    diferencia entre foreach y map

--- 10-Estructuras/js/03-app.js
+++ 10-Estructuras/js/03-app.js
@@ -11,3 +11,19 @@ for( let propiedad in auto){
 let tareas = ["estudiar", "bañarse", "cantar", "dormir"];
 
 tareas.forEach( (pendientes) => { console.log(pendientes) });
+
+tareas.forEach((pendientes, i) =>{ console.log(`La pendiente ${pendientes} tiene el index: ${i}.`) });
+
+let carrito = [
+    {nombre:"Monitor", precio:500},
+    {nombre:"TV", precio: 600},
+    {nombre:"Blu-Ray", precio: 300},
+    {nombre:"DVD", precio: 700},
+    {nombre:"Estufa", precio: 1500},
+    {nombre:"Microndas", precio: 800},
+    {nombre:"Plancha", precio: 700}
+];
+
+carrito.forEach( (productos) => { console.log(productos.nombre) });
+carrito.forEach( productos =>  console.log(productos) );
+carrito.map( productos =>  console.log(productos) );
