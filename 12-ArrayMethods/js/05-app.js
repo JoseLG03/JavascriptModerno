aa34a939b42d47dd05108054d12e306f4d1ccb86
JoseLG03 <antares069@gmail.com>
2021-02-04 21:58:17 -0600

    .find

---
+++ 12-ArrayMethods/js/05-app.js
@@ -0,0 +1,15 @@
+const meses = ['Enero', 'Febrero', "Marzo", 'Abril', 'Mayo', 'Junio', 'Julio'];
+
+const carrito = [
+    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
+    { nombre: 'Televisión', precio: 100 },
+    { nombre: 'Tablet', precio: 200 },
+    { nombre: 'Audifonos', precio: 300 },
+    { nombre: 'Teclado', precio: 400 },
+    { nombre: 'Celular', precio: 700 },
+    { nombre: "Laptop", precio:1500},
+    { nombre: "Pantalla", precio:4550}
+]
+
+let resultado = carrito.find( producto => producto.nombre === "Tablet");
+console.log(resultado);
