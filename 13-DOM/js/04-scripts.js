01a92a6807b03f0afb41f834c7d8b976b06e4c23
JoseLG03 <antares069@gmail.com>
2021-02-09 21:23:39 -0600

    textContent

---
+++ 13-DOM/js/04-scripts.js
@@ -0,0 +1,7 @@
+let header = document.querySelector("h1");
+
+console.log(header);
+
+//innerText, si en el CSS esta oculto no va a encontrar el texto.
+//textContent, si lo encuentra
+//innerHTML, se trae todo el HTML
