1982737661942fe5537f7cd693368cddaaa04ca7
JoseLG03 <antares069@gmail.com>
2021-02-09 21:06:44 -0600

    dif queryselector y getElementByID

---
+++ 13-DOM/js/03-scripts.js
@@ -0,0 +1,13 @@
+let form = document.getElementById("formulario");
+
+console.log(form);
+
+//query selector regresa un solo elemento, el primero que encuentra
+
+let card = document.querySelector(".card");
+
+console.log(card);
+
+let card2 = document.querySelector("section.destinos div.contenedor-cards div.card:nth-child(3)");
+
+console.log(card2);
