64848e17f0fe36343406159b42245b6cb3cccde3
JoseLG03 <antares069@gmail.com>
2021-02-10 21:53:50 -0600

    remove

---
+++ 13-DOM/js/06-scripts.js
@@ -0,0 +1,11 @@
+let x = document.querySelector("nav");
+
+console.log(x);
+
+//x.remove();
+
+const nav = document.querySelector(".navegacion");
+
+console.log(nav.children);
+
+//nav.removeChild(nav.children[3]);
